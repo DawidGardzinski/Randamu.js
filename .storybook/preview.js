777e9c04adb4282aecda804541b238b6d862b00b
Dawid <dawidgardzinski@gmail.com>
2020-05-02 03:29:47 +0200

    fix storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,7 @@ import { addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../src/theme/mainTheme';
 import GlobalStyles from 'theme/GlobalStyles';
+import StoryRouter from 'storybook-react-router';
 
 addDecorator((story) => (
   <ThemeProvider theme={theme}>
@@ -15,3 +16,4 @@ addDecorator((story) => (
     {story()}
   </>
 ));
+addDecorator(StoryRouter());
