4018541d69db02c522645c9cfda5878c942200d3
Dawid <dawidgardzinski@gmail.com>
2020-06-30 14:56:44 +0200

    deploy fix 4

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'views/Root';
 import 'localization/i18n';
-import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/">
-      <Root />
-    </HashRouter>
+    <Root />
   </React.StrictMode>,
   document.getElementById('root'),
 );
